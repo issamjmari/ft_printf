cc09706499b35e0942fd98492d7138f8a89ad390
ijmari <issamjmari098@gmail.com>
2021-11-22 19:56:33 +0100

    All norminette fixed . full project

--- ft_printf.h
+++ ft_printf.h
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijmari <ijmari@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 19:54:59 by ijmari            #+#    #+#             */
+/*   Updated: 2021/11/22 19:55:43 by ijmari           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# include <unistd.h>
+# include <stdlib.h>
 
-#include <unistd.h>
-#include <stdlib.h>
-int     ft_printf(const char *, ...);
-size_t  ft_strlen(const char *s);
-void    ft_putchar(char c, int *count);
-int    ft_putnbr(long long nb, int *count);
-void    *ft_putstr(char *s, int *count);
+int		ft_printf(const char *s, ...);
+size_t	ft_strlen(const char *s);
+void	ft_putchar(char c, int *count);
+int		ft_putnbr(long long nb, int *count);
+void	*ft_putstr(char *s, int *count);
 void	ft_print_voidp(int *count, char *temp);
 void	ft_print_hexa(char hexa, int *count, char *temp);
 void	ft_handle_voidp(unsigned long int i, int *count);
--- voidp_handling.c
+++ voidp_handling.c
@@ -1,20 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   voidp_handling.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijmari <ijmari@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 17:48:23 by ijmari            #+#    #+#             */
+/*   Updated: 2021/11/22 19:53:32 by ijmari           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-void	ft_print_voidp(unsigned long long i, int *count, char *temp)
+void	ft_print_voidp(int *count, char *temp)
 {
 	int					j;
 
 	j = ft_strlen (temp);
 	j--;
 	if (j >= 0)
-    {
-        ft_putchar ('0', count);
-        ft_putchar('x', count);
-        while (j >= 0)
-		    ft_putchar(temp[j--], count);
-    }
+	{
+		ft_putchar ('0', count);
+		ft_putchar('x', count);
+		while (j >= 0)
+			ft_putchar(temp[j--], count);
+	}
 }
-void	ft_handle_voidp(unsigned long long i, int *count)
+
+void	ft_handle_voidp(unsigned long int i, int *count)
 {
 	int					j;
 	char				*hex;
@@ -25,6 +38,8 @@ void	ft_handle_voidp(unsigned long long i, int *count)
 	if (i == 0)
 	{
 		temp[0] = hex[0];
+		ft_putchar('0', count);
+		ft_putchar('x', count);
 		ft_putchar(temp[0], count);
 	}
 	else
@@ -34,6 +49,7 @@ void	ft_handle_voidp(unsigned long long i, int *count)
 			temp[j++] = hex[i % 16];
 			i /= 16;
 		}
-		ft_print_voidp(i, count, temp);
+		temp[j] = '\0';
+		ft_print_voidp(count, temp);
 	}
 }
